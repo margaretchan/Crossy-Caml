f2da4265f20e2f6415019e3e54c36f6fd20a48d1
David Sun <ds833@cornell.edu>
2019-10-28 20:30:11 -0400

    Added line of code to test the Draw module

--- main.ml
+++ main.ml
@@ -2,7 +2,7 @@ open Graphics
 open Draw
 
 let rec display () = 
-  let () = Graphics.open_graph "" in
+  let () = Draw.draw () in
   match read_line () with
   | exception End_of_file -> display ()
   | x -> if x = "done" then () else display ()
