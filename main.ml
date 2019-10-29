d1de9a99b3713e7857d075ea0e627bd636ed1322
David Sun <ds833@cornell.edu>
2019-10-29 10:22:23 -0400

    Did nothing

--- main.ml
+++ main.ml
@@ -1,12 +1,19 @@
 open Graphics
 open Draw
 
-let rec display () = 
-  let () = Draw.draw () in
-  match read_line () with
-  | exception End_of_file -> display ()
-  | x -> if x = "done" then () else display ()
+(** [display_loop last_update_time fps] controls the window refresh updates 
+    [last_update_time] is the time the window was last refreshed 
+    [fps] is the number of update frames per second 
+    The game is exited by closing the window *)
+let rec display_loop last_update_time fps = 
+  if ((Sys.time ()) -. last_update_time > (1.0 /. fps)) 
+  then ( 
+    Draw.update_window ();
+    display_loop (Sys.time ()) fps
+  ) 
+  else display_loop last_update_time fps
 
-let _ = print_endline "hi"
-
-let () = display ()
+(** Initializes game *)
+let () = 
+  Draw.init_window ();
+  display_loop (Sys.time ()) 30.0
