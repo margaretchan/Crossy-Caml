38f020110c50304d411b76fa3dfff27eda4d97f8
Margaret Chan <mchan20822@gmail.com>
2019-11-11 17:10:32 -0500

    Fixes screen sig implmentation

--- screen.mli
+++ screen.mli
@@ -1,11 +1,11 @@
-module type Screen = sig 
-  include Queue
+module Screen : sig 
 
   val empty : Object.collidable list Queue.t 
 
   val collision_process : Object.collidable -> Object.collidable list Queue.t 
     -> State.state
 
-  val update : Object.collidable list Queue.t -> bool -> Object.collidable list Queue.t
+  (* val update : Object.collidable list Queue.t -> bool 
+     -> Object.collidable list Queue.t *)
 
 end
