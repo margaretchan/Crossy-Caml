996381c363a15a18d7770939572409a9d817ea16
Margaret Chan <mchan20822@gmail.com>
2019-11-16 17:12:55 -0500

    Game screen now only updates based on time
    - still has a slight flashing issue

--- draw.mli
+++ draw.mli
@@ -4,6 +4,9 @@ open Screen
 (** [init_window ()] opens the game window *)
 val init_window : unit -> unit
 
+(**[extract_obj c] extracts the Object from collidable [c] *)
+val extract_obj : Object.collidable -> Object.obj
+
 (** [update_window player_dir old_pos update_obstacles screen seq_bad_rows] 
     is the tuple of:
     1. player object after drawing the updated game graphics,
@@ -27,5 +30,3 @@ val start_page : unit -> unit
 (** [game_over ()] erases the current game window and draws the 
     game over screen *)
 val game_over : int -> int -> unit 
-
-val extract_obj : Object.collidable -> Object.obj
