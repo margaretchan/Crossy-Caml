7f50a35f444d5586bdd24418b032dfd13eb60a75
Margaret Chan <mchan20822@gmail.com>
2019-11-16 18:49:16 -0500

    Completes initial implementation of block side movement

--- screen.ml
+++ screen.ml
@@ -9,9 +9,13 @@ module Screen = struct
 
   let empty = create ()
 
-  let collision_process player s =
+  let obj_lst_from_tup tup = 
+    match tup with 
+    | lst, _ -> lst
+
+  let collision_process player (s : (Object.collidable list * bool) Queue.t) =
     try (
-      let bottom_list = peek s in
+      let bottom_list = obj_lst_from_tup (peek s) in
       (* filter the list to be only the blocks near the player *)
       let collide_with_player = Object.check_collision player in
       let collision_list = List.map collide_with_player bottom_list in
@@ -31,7 +35,7 @@ module Screen = struct
   (** [shift_down col_lst] is a unit. It modifies all the collidables in 
       [col_lst] to have their position shifted down by their height. 
       Requires: [col_lst] contains no players *)
-  let shift_down (col_lst : collidable list) =
+  let shift_down col_lst =
     let rec helper lst = 
       match lst with 
       | [] -> ()
@@ -39,32 +43,34 @@ module Screen = struct
           | Block (_, obj) -> obj.y_pos <- (obj.y_pos - obj.height)
           | Player _ -> failwith "List can't contain player");
         helper t in 
-    helper col_lst
+    helper (obj_lst_from_tup col_lst)
 
   (** [shift_side col_lst] is a unit. It modifies all the collidables in 
       [col_lst] to have their position shifted either left or right 
       (determined pseudo-randomly) by their width. 
       Requires: [col_lst] contains no players *)
-  let shift_side (col_lst : collidable list) = 
-    let move_dir = if (Random.bool ()) then -1 else 1 in 
-    let rec helper lst = 
+  let shift_side col_lst = 
+    let rec helper lst move_dir = 
       match lst with 
       | [] -> ()
       | b :: t -> begin
           match b with 
           | Block (_, obj) -> 
             obj.x_pos <- (obj.x_pos + (move_dir * obj.width)); 
-            helper t
+            helper t move_dir
           | Player _ -> failwith "List can't contain player"
         end in 
-    helper col_lst
+    match col_lst with 
+    | lst, dir -> 
+      let move_dir = if dir then 1 else -1 in 
+      helper lst move_dir
 
   (** [get_obj col_lst] is the object of the first colllidable of [col_lst]
       Requires: [col_lst] is not empty with no player collidables *)
   let get_obj collidable_lst = 
     match collidable_lst with
-    | [] -> failwith "List is empty"
-    | h :: t -> (match h with
+    | [], _ -> failwith "List is empty"
+    | h :: t, dir -> (match h with
         | Block (_, obj) -> obj
         | Player _ -> failwith "Should have no player in list")
 
